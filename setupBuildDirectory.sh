4a6120b0436dab4e647bd950b21df4834564be4e
Insa Kröger <insa.kroeger@outlook.com>
2017-03-27 11:36:13 +0200

    Adding shell script for setting up build directory (linux-based)

---
+++ setupBuildDirectory.sh
@@ -0,0 +1,11 @@
+mkdir -p build
+
+mkdir -p build/wav
+mkdir -p build/text
+mkdir -p build/TextGrid
+
+cp ../VoiceBuilding/resources/wav/*.wav build/wav/
+cp ../VoiceBuilding/resources/text/*.txt build/text/
+cp ../VoiceBuilding/resources/MAUS_TextGrids/*.TextGrid build/TextGrid/
+
+
