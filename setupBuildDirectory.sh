8edd75c7063e2a16b55d53af1250ceeccf9332d0
Insa Kröger <insa.kroeger@outlook.com>
2017-03-29 10:26:29 +0200

    Fixing shell script

--- setupBuildDirectory.sh
+++ setupBuildDirectory.sh
@@ -12,9 +12,9 @@ cp -n ../VoiceBuilding/resources/text/*.txt build/text/
 cp -n ../VoiceBuilding/resources/MAUS_TextGrids_combined/*.TextGrid build/TextGrid/
 
 ## removing files that create problems
-rm -f build/text/arctic_a0282.txt build/text/arctic_a0309.txt build/text/arctic_a0402.txt build/text/arctic_a0403.txt build/text/arctic_a0589.txt build/text/arctic_b0132.txt build/text/arctic_b0152.txt 
+rm -f  build/text/arctic_a0277.txt build/text/arctic_b0152.txt build/text/arctic_a0282.txt build/text/arctic_a0309.txt build/text/arctic_a0402.txt build/text/arctic_a0403.txt build/text/arctic_a0589.txt build/text/arctic_b0132.txt build/text/arctic_b0152.txt 
 
-rm -f build/wav/arctic_a0282.wav build/wav/arctic_a0309.wav build/wav/arctic_a0402.wav build/wav/arctic_a0403.wav build/wav/arctic_a0589.wav build/wav/arctic_b0132.wav build/wav/arctic_b0152.wav 
+rm -f build/wav/arctic_a0277.wav build/wav/arctic_a0282.wav build/wav/arctic_a0309.wav build/wav/arctic_a0402.wav build/wav/arctic_a0403.wav build/wav/arctic_a0589.wav build/wav/arctic_b0132.wav build/wav/arctic_b0152.wav 
 
-rm -f build/TextGrid/arctic_a0282.TextGrid build/TextGrid/arctic_a0589.TextGrid build/TextGrid/arctic_b0132.TextGrid build/TextGrid/arctic_b0152.TextGrid 
+rm -f build/TextGrid/arctic_a0277.TextGrid build/TextGrid/arctic_a0282.TextGrid build/TextGrid/arctic_a0589.TextGrid build/TextGrid/arctic_b0132.TextGrid build/TextGrid/arctic_b0152.TextGrid 
 
