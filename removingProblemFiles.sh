76587bceaac6377ec68bbb67b3d9df07c0cdc693
Insa Kröger <insa.kroeger@outlook.com>
2017-03-29 15:40:48 +0200

    Adapting build.gradle -> metadata is now downloaded automatically

---
+++ removingProblemFiles.sh
@@ -0,0 +1,7 @@
+## removing files that create problems
+rm -f  build/text/arctic_a0276.txt build/text/arctic_a0277.txt build/text/arctic_b0152.txt build/text/arctic_a0282.txt build/text/arctic_a0309.txt build/text/arctic_a0402.txt build/text/arctic_a0403.txt build/text/arctic_a0589.txt build/text/arctic_b0132.txt build/text/arctic_b0152.txt 
+
+rm -f  build/wav/arctic_a0276.wav build/wav/arctic_a0277.wav build/wav/arctic_a0282.wav build/wav/arctic_a0309.wav build/wav/arctic_a0402.wav build/wav/arctic_a0403.wav build/wav/arctic_a0589.wav build/wav/arctic_b0132.wav build/wav/arctic_b0152.wav 
+
+rm -f build/TextGrid/arctic_a0276.TextGrid build/TextGrid/arctic_a0277.TextGrid build/TextGrid/arctic_a0282.TextGrid build/TextGrid/arctic_a0589.TextGrid build/TextGrid/arctic_b0132.TextGrid build/TextGrid/arctic_b0152.TextGrid 
+
